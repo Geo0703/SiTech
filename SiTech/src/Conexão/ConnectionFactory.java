2b58d5c986e111eea71c21bb29278e74428a875c
Geovane augusto <Geovaneaugusto0703@gmail.com>
2024-06-11 17:59:06 -0300

    Add files via upload

---
+++ SiTech/src/Conexão/ConnectionFactory.java
@@ -0,0 +1,39 @@
+
+package Conexão;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+        
+public class ConnectionFactory {
+    private String usuario = "root";
+    private String senha = "Root";
+    private String host = "localhost";
+    private String porta = "3306";
+    private String bd = "Sitech";
+
+    public ConnectionFactory() {
+        this.bd = "Sitech";
+    }
+
+    public Connection obtemConexao() throws SQLException {
+        try {
+            Connection c = DriverManager.getConnection(
+                    "jdbc:mysql://" + host + ":" + porta + "/" + bd
+                    + "?useTimezone=true&serverTimezone=UTC",
+                    usuario, senha);
+            System.out.println("Conexão efetuada com sucesso!!");
+            return c;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Erro na conexão com o banco de dados!");
+            return null;
+            
+        }
+
+    }
+
+}
+
